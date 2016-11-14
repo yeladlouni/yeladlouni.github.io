d486df4cbebf851fdbbede00ea22afe890eb4ec8
yeladlouni <yeladlouni@gmail.com>
2016-11-14 00:39:03 +0000

    settings changes

--- appcompose/home/home.js
+++ appcompose/home/home.js
@@ -3,6 +3,7 @@
 */
 
 // The initialize function must be run each time a new page is loaded
+
 Office.initialize = function (reason) {
 	$(document).ready(function () {
 		$('#set-data').click(writeText);
